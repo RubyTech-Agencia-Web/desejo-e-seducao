ff1a8d5d909ccd388ced59671fbf88ad0c36d291
Edilando <35576130+EdilandoSaturnino@users.noreply.github.com>
2022-04-24 20:17:56 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tc =>{
+            tc.classList.remove('produto__ativado')
+        })
+        target.classList.add('produto__ativado')
+
+        tabs.forEach(t =>{
+            t.classList.remove('produto-tab-ativado')
+        })
+        tab.classList.add('produto-tab-ativado')
+    })
+})
+
+/*=============== CARROSSEL  ===============*/
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
